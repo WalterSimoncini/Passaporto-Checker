28bb9bd42af4caafc2e8b0e8e03a40e9f0c2bd13
Walter Simoncini <walterdev@icloud.com>
2019-01-07 19:39:51 +0100

    Added a tool that kills leftover chrome instances

---
+++ kill_leftover_chrome_instances.py
@@ -0,0 +1,18 @@
+# This program kills instances of Chrome that may have been left over
+# by crashes of the main script
+
+import os
+import signal
+import subprocess
+
+def kill_chrome_instances():
+    p = subprocess.Popen(['ps', '-A'], stdout = subprocess.PIPE)
+    out, err = p.communicate()
+
+    if err == None:
+        for line in out.splitlines():
+            if "chrome" in line:
+                pid = pid = int(line.split(None, 1)[0])
+                os.kill(pid, signal.SIGKILL)
+
+kill_chrome_instances()
